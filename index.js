02f82e6e1fe3e6883a668bf2d9f88e5fdc9c035d
Muhammad Ahmad Mahmood <ahmadmahmood296@gmail.com>
2024-03-27 23:26:49 +0500

    update :🚨 Unhandled endpoints

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ const PORT = process.env.PORT || 3000;
 
 connectToMongo();
 
+
 app.use('/api' , require('./routes/user'));
+
+app.use((req, res) => {
+  res.status(404).send('Page not found');
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on  http://localhost ${PORT}`);
